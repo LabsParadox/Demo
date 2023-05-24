45a9f89d3d7af31b0321cf184d42da9aad7fe0b4
Matt Martino <matt@prdxlabs.com>
2023-05-24 00:42:19 -0700

    Implemented React Router 5.2.0 and Concurrently

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-// App.js
 import React from 'react';
-import './styles.css';
 import Dashboard from './components/Dashboard';
+import { Route, Switch, Link } from "react-router-dom";
+
+import Home from './Home';
+import About from './About';
+
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Switch>
+      
       <Dashboard />
     </div>
   );
